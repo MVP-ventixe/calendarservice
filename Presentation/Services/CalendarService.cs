260cdaeff0d0ff01b27d19191f60c0b3c32099a4
ThorowZ <dennisgranatdennisgranat@hotmail.com>
2025-06-11 00:24:50 +0200

    Stuck on Calendar not wanting to generate a CalendarGrpc.

--- Presentation/Services/CalendarService.cs
+++ Presentation/Services/CalendarService.cs
@@ -1,10 +1,30 @@
 ﻿using System.Globalization;
-using Presentation;
+using Grpc.Core;
 
 namespace Presentation.Services;
 
-public class CalendarService : Calend
+public class CalendarService : CalendarHandler.CalendarHandlerBase
 {
 
+    private List<Calendar> _calendars = new List<Calendar>
+    {
+        new Calendar
+        {
+            Id = "1",
+            name = "Meeting with Bob",
+            description = "Discuss project updates",
+            date = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd"), 
+            time = DateTime.Now.AddHours(2).ToString("HH:mm")     
+        }
+    };
 
+    public override Task<GetCalendarResponse> GetCalendar(GetCalendarRequest request, ServerCallContext context)
+    {
+        var response = new GetCalendarResponse();
+        response.Calendars.AddRange(_Calendars);
+        
+
+    }
+
+    public override Task<CreateCalendarRequest>  
 }
